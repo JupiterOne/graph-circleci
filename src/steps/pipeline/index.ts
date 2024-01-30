3aad7526dc317a94ffa355687e6a5c91f5085a3e
Gaston Yelmini <gastonyelmini@gmail.com>
2024-01-30 11:04:44 -0300

    INT-8998: add permission issue warning

--- src/steps/pipeline/index.ts
+++ src/steps/pipeline/index.ts
@@ -1,12 +1,15 @@
 import {
   IntegrationStep,
   IntegrationStepExecutionContext,
+  IntegrationWarnEventName,
+  getRawData,
 } from '@jupiterone/integration-sdk-core';
 
 import { createAPIClient } from '../../client';
 import { IntegrationConfig } from '../../config';
 import { Entities, Steps } from '../constants';
 import { createPipelineEntity, getPipelineKey } from './converter';
+import { CircleCIUserGroup } from '../../types';
 
 export async function fetchPipelines({
   instance,
@@ -15,31 +18,52 @@ export async function fetchPipelines({
 }: IntegrationStepExecutionContext<IntegrationConfig>) {
   const apiClient = createAPIClient(instance.config, logger);
 
+  const inaccessibleOrganizations: string[] = [];
+
   await jobState.iterateEntities(
     { _type: Entities.USER_GROUP._type },
     async (userGroupEntity) => {
       if (userGroupEntity.slug) {
-        await apiClient.iteratePipelines(
-          userGroupEntity.slug.toString(),
-          async (pipeline) => {
-            // We have seen instances where the API response has the same Pipeline
-            // more than once.  Documentation confirms the pipeline ID is a UUID,
-            // so we can safely treat these as duplicates and skip adding them.
-            const pipelineKey = getPipelineKey(pipeline.id);
-            if (!jobState.hasKey(pipelineKey)) {
-              const pipelineEntity = createPipelineEntity(pipeline);
-              await jobState.addEntity(pipelineEntity);
-            } else {
-              logger.info(
-                { pipelineKey },
-                `Encountered a duplicate pipeline key.  Skipping.`,
-              );
-            }
-          },
-        );
+        try {
+          await apiClient.iteratePipelines(
+            userGroupEntity.slug.toString(),
+            async (pipeline) => {
+              // We have seen instances where the API response has the same Pipeline
+              // more than once.  Documentation confirms the pipeline ID is a UUID,
+              // so we can safely treat these as duplicates and skip adding them.
+              const pipelineKey = getPipelineKey(pipeline.id);
+              if (!jobState.hasKey(pipelineKey)) {
+                const pipelineEntity = createPipelineEntity(pipeline);
+                await jobState.addEntity(pipelineEntity);
+              } else {
+                logger.info(
+                  { pipelineKey },
+                  `Encountered a duplicate pipeline key.  Skipping.`,
+                );
+              }
+            },
+          );
+        } catch (e) {
+          if (e.code === 404) {
+            const userGroup = getRawData<CircleCIUserGroup>(userGroupEntity);
+
+            inaccessibleOrganizations.push(userGroup?.slug as string);
+          } else {
+            throw e;
+          }
+        }
       }
     },
   );
+
+  if (inaccessibleOrganizations.length > 0) {
+    logger.publishWarnEvent({
+      name: IntegrationWarnEventName.MissingPermission,
+      description: `Could not fetch pipelines from the following using the following org slugs: ${inaccessibleOrganizations.join(
+        ', ',
+      )}). Please make sure to use personal API tokens with the right access permissions.`,
+    });
+  }
 }
 
 export const pipelinesSteps: IntegrationStep<IntegrationConfig>[] = [
